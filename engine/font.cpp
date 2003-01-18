3a66dfffd09ef183d350a5797ea6f1b95f153aa7
Andy Friesen <andy.friesen@gmail.com>
2003-01-18 20:15:18 +0000

    engine no longer crashes if you try to PrintString a newline.

--- engine/font.cpp
+++ engine/font.cpp
@@ -13,9 +13,9 @@ static const char subsetmarker='~';
 
 void CFont::Free()
 {
-    int nSetsize=set.size();
-    for (int nSet=0; nSet<nSetsize; nSet++)
-        for (int nGlyph=0; nGlyph<96; nGlyph++)
+    int nSetsize = set.size();
+    for (int nSet = 0; nSet < nSetsize; nSet++)
+        for (int nGlyph = 0; nGlyph < 96; nGlyph++)
             gfxFreeImage(set[nSet].glyph[nGlyph]);
         
         set.clear();
@@ -31,21 +31,21 @@ bool CFont::LoadFNT(const char* filename)
     
     set.resize(f.NumSubSets());
     
-    nWidth=nHeight=0;
-    nTabsize=30;
+    nWidth = nHeight = 0;
+    nTabsize = 30;
     
     // eep @_@
-    for (int nSet=0; nSet<f.NumSubSets(); nSet++)
+    for (int nSet = 0; nSet < f.NumSubSets(); nSet++)
     {
-        for (int nGlyph=0; nGlyph<96; nGlyph++)
+        for (int nGlyph = 0; nGlyph < 96; nGlyph++)
         {
-            CFontFile::SSubSet& s=f.GetSubSet(nSet);
+            CFontFile::SSubSet& s = f.GetSubSet(nSet);
             
-            int nGlyphidx=s.nGlyphtbl[nGlyph+32];
+            int nGlyphidx = s.nGlyphtbl[nGlyph + 32];
             
-            set[nSet].glyph[nGlyph]=gfxCreateImage(1,1);
+            set[nSet].glyph[nGlyph] = gfxCreateImage(1, 1);
             
-            CPixelMatrix& glyph=f.GetGlyph(nGlyphidx);
+            CPixelMatrix& glyph = f.GetGlyph(nGlyphidx);
             
             gfxCopyPixelData(
                 set[nSet].glyph[nGlyph],
@@ -55,95 +55,94 @@ bool CFont::LoadFNT(const char* filename)
                 );
             
             
-            if (nWidth<glyph.Width())				nWidth=glyph.Width();
-            if (nHeight<glyph.Height())				nHeight=glyph.Height();
+            if (nWidth < glyph.Width())     nWidth = glyph.Width();
+            if (nHeight < glyph.Height())   nHeight = glyph.Height();
         }
     }
     
     return true;
 }
 
-void CFont::PrintChar(int& x,int y,int cursubset,char c)
+void CFont::PrintChar(int& x, int y, int cursubset, char c)
 {
-    c-=32;
+    c -= 32;
     
-    if (c<0 || c>96)
+    if (c < 0 || c > 96)
         return;
     
-    handle h=set[cursubset].glyph[c];
-    gfxBlitImage(h,x,y,true);
-    x+=gfxImageWidth(h);
+    handle h = set[cursubset].glyph[c];
+    gfxBlitImage(h, x, y, true);
+    x += gfxImageWidth(h);
 }
 
-void CFont::PrintString(int startx,int starty,const char* s)
-{	
+void CFont::PrintString(int startx, int starty, const char* s)
+{   
     int cursubset=0;
     int x=startx;
     int y=starty;
     
-    for (unsigned int i=0; i<strlen(s); i++)
+    for (uint i=0; i<strlen(s); i++)
     {
         switch (s[i])
         {
-        case '\n':																// newline
-            y+=gfxImageHeight(nHeight);
-            x=startx;
+        case '\n':          // newline
+            y += nHeight;
+            x = startx;
             break;
             
-        case '\t':																// tab
-            x-=startx;
-            x+=nTabsize-x%nTabsize;
-            x+=startx;
+        case '\t':          // tab
+            x += nTabsize - (x - startx) % nTabsize;
             break;
             
         case subsetmarker:
             i++;
             
-            if (i>=strlen(s))
-                return;	// subset marker at end of string.  bjork.
+            if (i >= strlen(s))
+                return; // subset marker at end of string.  bjork.
             
-            if (s[i]>='0' && s[i]<=set.size()+'0')								// number?  switch the subset. (also make sure that it's a valid subset index
-                cursubset=s[i]-'0';
-            else if (s[i]==subsetmarker)
-                PrintChar(x,y,cursubset,s[i]);
+            if (s[i] >= '0' && s[i] <= '0' + set.size())                    // number?  switch the subset. (also make sure that it's a valid subset index
+                cursubset=s[i] - '0';
+            else if (s[i] == subsetmarker)
+                PrintChar(x, y, cursubset, s[i]);
             break;
             
         default:
-            PrintChar(x,y,cursubset,s[i]);
+            PrintChar(x, y, cursubset, s[i]);
         }
     }
 }
 
 int CFont::StringWidth(const char* s) const
 {
-    int nWidth=0;
-    int nCursubset=0;
+    int nWidth = 0;
+    int nCursubset = 0;
+    uint len = strlen(s);
     
-    for (uint i=0; i<strlen(s); i++)
+    for (uint i = 0; i < len; i++)
     {
-        unsigned char c=s[i];
+        unsigned char c = s[i];
         
         switch (c)
         {
         case '\n':
-            return nWidth;														// um.. @_x
+            return nWidth;                                                          // um.. @_x
             
-        case '\t':																// tab
-            nWidth+=nTabsize-nWidth%nTabsize;
+        case '\t':                                                                  // tab
+            nWidth += nTabsize - nWidth % nTabsize;
             break;
             
         case subsetmarker:
             i++;
-            if (s[i]==subsetmarker)
-                nWidth+=gfxImageWidth(set[nCursubset].glyph[subsetmarker-32]);
-            else if (s[i]>='0' && s[i]<='0'+set.size())								// valid subset number?
-                nCursubset=s[i]-'0';
+            if (s[i] == subsetmarker)
+                nWidth += gfxImageWidth(set[nCursubset].glyph[subsetmarker - 32]);
+            else if (s[i] >= '0' && s[i] <= '0' + set.size())                       // valid subset number?
+                nCursubset = s[i] - '0';
             break;
             
         default:
-            if (c<32 || c>32+96)
-                continue;															// invalid char, skip it.
-            nWidth+=gfxImageWidth(set[nCursubset].glyph[c-32]);
+            if (c < 32 || c > 32 + 96)
+                continue;                                                           // invalid char, skip it.
+            nWidth += gfxImageWidth(set[nCursubset].glyph[c - 32]);
         }
     }
     return nWidth;
