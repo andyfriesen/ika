32efd683e715396b6472ce58885cced27fb58eb5
khross <khross@4d5f71c2-9d0d-0410-8b67-f93bfe66ef41>
2003-02-04 02:29:33 +0000

    Added docstrings to the Font object

--- engine/script/FontObject.cpp
+++ engine/script/FontObject.cpp
@@ -14,10 +14,26 @@ namespace Script
 
         PyMethodDef methods[] =
         {
-            {   "Print",        (PyCFunction)Font_Print,        1   },
-            {   "CenterPrint",  (PyCFunction)Font_CenterPrint,  1   },
-            {   "RightPrint",   (PyCFunction)Font_RightPrint,   1   },
-            {   "StringWidth",  (PyCFunction)Font_StringWidth,  1   },
+            {   "Print",        (PyCFunction)Font_Print,        METH_VARARGS,
+                 "font.Print(x, y, text)\n"
+                 "Prints a string of text on screen at (x,y)."
+            },
+            {   "CenterPrint",  (PyCFunction)Font_CenterPrint,  METH_VARARGS,
+                "font.CenterPrint(x, y, text)\n"
+                "Prints a string of text on screen,\n"
+                "with x as the center point rather than the leftmost point."
+
+            },
+            {   "RightPrint",   (PyCFunction)Font_RightPrint,   METH_VARARGS,
+                "font.RightPrint(x, y, text)\n"
+                "Prints a string of text on screen,\n"
+                "with x as the center point rather than the rightmost point."
+            },
+            {   "StringWidth",  (PyCFunction)Font_StringWidth,  METH_VARARGS,
+                "font.StringWidth(text)\n"
+                "Returns how many pixels in width the passed string would be,\n"
+                "if printed in this font."
+            },
             {   NULL,           NULL    }
         };
 
