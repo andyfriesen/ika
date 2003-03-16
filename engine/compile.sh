3284762815673a7fdea4221f2d3dcc6c73e37332
Andy Friesen <andy.friesen@gmail.com>
2003-03-16 21:21:34 +0000

    Tweaked the linux compile script some.

--- engine/compile.sh
+++ engine/compile.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+PYTHONVERSION=`python -c "import sys; print sys.version[:3]"`
+PYTHONDIR=-I/usr/include/python$PYTHONVERSION
+CFLAGS="-I. -I.. -I/usr/X11R6/include `sdl-config --cflags` $PYTHONDIR"
+LDFLAGS="`sdl-config --libs` -L/usr/X11R6/lib -L -lstdc++ -lz -lcorona -laudiere -lutil -lGL -lGLU"
+
 g++ \
-   {FPSCounter,entity,font,graph,input,main,script,sound,sprite,tileset,timer}.cpp \
+{FPSCounter,entity,font,graph,input,main,script,sound,sprite,tileset,timer}.cpp \
    opengl/{Driver,Image}.cpp \
    script/*.cpp \
    ../common/{Canvas,chr,configfile,fileio,fontfile,log,map,misc,rle,vergepal,vsp}.cpp \
-   -I.  -I..  \
-   `sdl-config --cflags --libs` \
-   -I/usr/include/python`python -c "import sys; print sys.version[:3]"` \
-   -lz -lcorona -laudiere -lutil -lGL -lGLU \
+   /usr/lib/python$PYTHONVERSION/config/libpython$PYTHONVERSION.a \
+   $CFLAGS $LDFLAGS \
    -o ika
