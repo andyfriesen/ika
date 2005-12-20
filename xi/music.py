22f0d8ece254a00f9c80398c96e6b33d1f86e3f9
escovan <escovan@4d5f71c2-9d0d-0410-8b67-f93bfe66ef41>
2005-12-20 05:28:35 +0000

    Replaced ika.Sound with the appropriate ika.Music.  Also replace str with basestring.

--- xi/music.py
+++ xi/music.py
@@ -1,42 +1,41 @@
-'''
-Music handler... thingie.
-'''
+"""Music handler."""
 
 import ika
+
 
 _cache_size = 4
 _cache = []
 
+
 def stopMusic():
     if len(_cache) > 0:
-        fname, sound = _cache[0]
-        sound.Pause()
+        filename, music = _cache[0]
+        music.Pause()
 
-def cacheMusic(fileName):
-    assert isinstance(fileName, str)
 
+def cacheMusic(filename):
+    assert isinstance(filename, basestring)
     i = 0
     while i < len(_cache):
-        name, sound = _cache[i]
-        if name == fileName:
-            # move it to the head of the list
+        name, music = _cache[i]
+        if name == filename:
+            # Move it to the head of the list.
             _cache.pop(i)
-            _cache.insert(0, (name, sound))
+            _cache.insert(0, (name, music))
             return sound
         i += 1
-
-    # if execution has reached this point, then we have to load it now
+    # If execution has reached this point, then we have to load it now.
     while len(_cache) > _cache_size:
-        _cache.pop() # discard the oldest
+        # Discard the oldest.
+        _cache.pop()
+    music = ika.Music(filename)
+    music.loop = True
+    _cache.insert(0, (filename, music))
+    return music
 
-    sound = ika.Sound(fileName)
-    sound.loop = True
-    _cache.insert(0, (fileName, sound))
-    return sound
 
 def playMusic(fileName):
     stopMusic()
-
-    sound = cacheMusic(fileName)
-    assert sound is not None
-    sound.Play()
+    music = cacheMusic(filename)
+    assert music is not None
+    music.Play()
