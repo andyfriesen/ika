241f86dce91ae124d40ebbbd0e6fe52e3d5b87ca
escovan <escovan@4d5f71c2-9d0d-0410-8b67-f93bfe66ef41>
2005-12-21 22:55:46 +0000

    Replaced with a newer version.

--- xi/statset.py
+++ xi/statset.py
@@ -1,9 +1,15 @@
+#!/usr/bin/env python
+
+import xi
+
+
 class StatSet(object):
     __slots__ = ['hp', 'mp', 'maxhp', 'maxmp', 'atk', 'grd', 'hit', 'eva',
                  'str', 'vit', 'mag', 'wil', 'spd', 'luk', 'exp']
 
     def __init__(self, hp=0, mp=0, atk=0, grd=0, hit=0, eva=0, str=0, vit=0,
                  mag=0, wil=0, spd=0, luk=0, exp=0):
+        super(StatSet, self).__init__()
         self.hp = hp
         self.mp = mp
         self.maxhp = hp
@@ -20,13 +26,21 @@ class StatSet(object):
         self.luk = luk
         self.exp = exp
 
-    def Clone(self):
+    def cap(self):
+        # Clamp HP/MP to 0-999 (inclusive.)
+        for attr in self.__slots__[:4]:
+            setattr(self, attr, xi.clamp(getattr(self, attr), 0, 9999))
+        # All other stats capped to 0-99.
+        for attr in self.__slots__[4:]:
+            setattr(self, attr, xi.clamp(getattr(self, attr), 0, 99))
+
+    def clone(self):
         result = StatSet()
         for variable in self.__slots__:
             result.__dict__[variable] += self.__dict__[variable]
         return result        
 
-    def __iadd__(self, rhs):
+    def __iadd__(self, other):
         for variable in self.__slots__:
             self.__dict__[variable] += other.__dict__[variable]
         return self
@@ -36,7 +50,7 @@ class StatSet(object):
         result += other
         return result
 
-    def __isub__(self, rhs):
+    def __isub__(self, other):
         for variable in self.__slots__:
             self.__dict__[variable] -= other.__dict__[variable]
         return self
