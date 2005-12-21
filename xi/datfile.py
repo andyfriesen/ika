2ba02eb9c69e7ddaa0f0f716c269141bf9515556
escovan <escovan@4d5f71c2-9d0d-0410-8b67-f93bfe66ef41>
2005-12-21 02:41:34 +0000

    Obsoleted module in deference to xi.parser.

--- xi/datfile.py
+++ xi/datfile.py
@@ -1,12 +1,1 @@
-#!/usr/bin/env python
-
-def Read(string):
-    starting_position = string.find('(')
-    ending_position = string.rfind(')')
-    if starting_position != -1 and ending_position != -1:
-        return [string[:starting_position].split() +
-                Read(string[starting_position + 1 : ending_position]) +
-                string[ending_position + 1:].split()]
-            
-    else:
-        return [string.split()]
+# This module is obsolete.
