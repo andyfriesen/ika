1ead06fafa3d947af967358401b6d490b1af9f84
escovan <escovan@4d5f71c2-9d0d-0410-8b67-f93bfe66ef41>
2007-02-05 04:08:19 +0000

    Inlined GetTime function.

--- engine/timer.h
+++ engine/timer.h
@@ -1,10 +1,9 @@
-
-#ifndef TIMER_H
-#define TIMER_H
+#pragma once
+#include <SDL/SDL.h>
 
 // Ticks in a second.
 const int timeRate = 100;
 
-int GetTime();
-
-#endif
+inline int GetTime() {
+    return SDL_GetTicks() * timeRate / 1000;
+}
