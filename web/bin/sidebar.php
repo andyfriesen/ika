302b53bcef552d4d81ba2be61399d5911b6d246e
escovan <escovan@4d5f71c2-9d0d-0410-8b67-f93bfe66ef41>
2007-04-26 03:31:36 +0000

    made changes to sidebar.php for future stylistic effects

--- web/bin/sidebar.php
+++ web/bin/sidebar.php
@@ -1,32 +1,35 @@
-<div class="sidebar">
-    <div class="menu">
+<div class="sidebar" id="sidebar">
+    <div class="menu" id="menu">
         <a href="news.php">News</a><br />
         <a href="gallery.php">Gallery</a><br />
-        <a href="articles.php?view=1"><abbr title="Frequently Asked Questions">faq</abbr></a><br />
+        <a href="articles.php?view=1"><abbr title="Frequently Asked Questions">FAQ</abbr></a><br />
         <a href="articles.php">Articles</a><br />
         <a href="files.php">Files</a><br />
         <a href="links.php">Links</a><br />
         <a href="forum.php">Forum</a>
-        <?php if ($admin == True): ?>
+        <?php if ($admin == TRUE) { ?>
             <hr />
-            <a href="board.php">Forum-Old</a>
+            <a href="board.php">Old forum</a>
             <hr />
-            <a href="news.php?add=1">Add News</a><br />
-            <a href="news.php?edit=1">Edit News</a><br />
-            <a href="articles.php?queued=1">Q&rsquo;d Articles</a><br />
-            <a href="files.php?queued=1">Q&rsquo;d Files</a><br />
-            <a href="links.php?queued=1">Q&rsquo;d Links</a><br />
-            <a href="admin.php">More Admin</a>
-        <?php endif; if (isset($_username)): ?>
+            <a href="news.php?add=1">Add news</a><br />
+            <a href="news.php?edit=1">Edit news</a><br />
+            <a href="articles.php?queued=1">Q&rsquo;d articles</a><br />
+            <a href="files.php?queued=1">Q&rsquo;d files</a><br />
+            <a href="links.php?queued=1">Q&rsquo;d links</a><br />
+            <a href="admin.php">More admin</a>
+        <?php } if (isset($_username)) { ?>
             <hr />
-            <?php echo "<b>" . $_username . "</b><br /><br />"; ?>
-            <a href="profile.php">Edit Profile</a><br />
-            <a href="login.php?logout=1">Log Out</a>
-        <?php elseif (!isset($_username)): ?>
+            <strong id="username"><?php echo $_username ?></strong><br />
+            <a href="profile.php">Edit profile</a><br />
+            <a href="login.php?logout=1">Log out</a>
+        <?php } elseif (!isset($_username)) { ?>
             <hr />
             <a href="register.php">Register</a><br />
-            <a href="login.php">Log In</a>
-        <?php endif; ?>
+            <a href="login.php">Log in</a>
+        <?php } ?>
     </div>
-    <div style="text-align: right"><a href="http://sourceforge.net">
-    <img src="http://sourceforge.net/sflogo.php?group_id=38780&amp;type=1" width="88" height="31" alt="SourceForge.net Logo" /></a></div></div>
+    <a href="http://sourceforge.net" id="sflogo">
+        <img src="http://sourceforge.net/sflogo.php?group_id=38780"
+             width="88" height="31" alt="SourceForge.net Logo" />
+    </a>
+</div>
