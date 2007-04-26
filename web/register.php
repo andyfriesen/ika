8a80fc4e55b68602418d2a883bd1a067194e2e4b
escovan <escovan@4d5f71c2-9d0d-0410-8b67-f93bfe66ef41>
2007-04-26 02:20:16 +0000

    Removed post/get globals from register.php

--- web/register.php
+++ web/register.php
@@ -4,65 +4,53 @@ include "bin/main.php";
 
 GenerateHeader("Register");
 
-do
-{
-    if (isset($register))
-    {
-        $Username = htmlspecialchars($Username);
+do {
+    if (isset($_GET['register'])) {
+        $Username = htmlspecialchars($_POST['Username']);
 
-        $result = mysql_query("SELECT count(*) AS the_count FROM users WHERE name='$Username'");
+        $result = mysql_query('SELECT count(*) AS the_count FROM users ' .
+                              "WHERE name='$Username'");
         $row = mysql_fetch_array($result);
-        if ($row["the_count"] > 0)
-        {
-            Error("A user by that name already exists.");
-            unset($register);
+        if ($row['the_count'] > 0) {
+            Error('A user by that name already exists.');
+            unset($_GET['register']);
             break;
         }
 
-        if ($Password != $Retype)
-        {
-            Error("The passwords don't match!  Please try again.");
-            unset($register);
+        if ($_POST['Password'] != $_POST['Retype']) {
+            Error('The passwords don\'t match! Please try again.');
+            unset($_GET['register']);
             break;
         }
 
-        $query = "INSERT INTO users (name, passwd) "
-               . "values ('$Username', sha1('$Password'))";
-        $result = mysql_query($query);
-        if (!$result)
-        {
+        $result = mysql_query('INSERT INTO users (name, passwd) ' .
+                              "values ('$Username', sha1('$_POST[Password]'))");
+        if (!$result) {
             StartBox("Notice");
             MySQL_NonFatalError();
             EndBox();
-            unset($register);
+            unset($_GET['register']);
             break;
         }
 
-        // Success!
-        StartBox("Notice");
-        echo "<p>You are registered now.</p>";
-        echo '<p><a href="index.php">Return to the main page.</a></p>';
+        StartBox('Notice');
+        ?>
+<p>You are registered now.</p>
+<p><a href="index.php">Return to the main page.</a></p>
+        <?php
         EndBox();
     }
 }
-while (False);
+while (FALSE);
 
-if (!isset($register))
-{
-    if (!isset($Username))
-        $Username = "";
-    if (!isset($Password))
-        $Password = "";
-    if (!isset($Retype))
-        $Retype = "";
-
-    StartBox("Registration Form");
-    CreateForm("$PHP_SELF?register=1",
-        "Username", "input",    $Username,
-        "Password", "password", $Password,
-        "Retype",   "password", $Retype,
-        "Submit",   "submit",   ""
-    );
+if (!isset($_GET['register'])) {
+    $username = isset($_GET['Username']) ? $_GET['Username'] : '';
+    $password = isset($_GET['Password']) ? $_GET['Password'] : '';
+    $retype = isset($_GET['Retype']) ? $_GET['Retype'] : '';
+    StartBox('Registration Form');
+    CreateForm("$PHP_SELF?register=1", 'Username', 'input', $username,
+               'Password', 'password', $password, 'Retype', 'password',
+               $retype, 'Submit', 'submit', '');
     EndBox();
 }
 
